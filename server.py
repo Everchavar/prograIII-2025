ff28722e202f0a63e2ac56c05ee816261e821bfe
Everchavar <everchavarria07@gmail.com>
2025-09-16 08:00:03 -0600

    se enviaron datos desde el servidor

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib import parse
 port = 3000
 
 class miServidor(SimpleHTTPRequestHandler):
@@ -6,6 +7,18 @@ class miServidor(SimpleHTTPRequestHandler):
         if self.path =="/":
             self.path= "/index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+        def do_POST(self):
+            longitud = int(self.headrs['content-Legth'])
+            datos = self.rfile.read(longitud)
+            datos = datos.decode ("utf-8")
+            datos = parse.unquote (datos)
+            print (datos)
+
+            self.send_response(200)
+            self.end_header()
+            self.wfile.write(datos.encode("utf-8"))
+            
        
 print("Servidor ejecutandose en el puerto 3000", port)
 server= HTTPServer(("localhost",port), miServidor)
