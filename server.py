3a467245980947c784a38ccd6082dc605027d3e5
Everchavar <everchavarria07@gmail.com>
2025-09-16 07:09:35 -0600

    servidor

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+port = 3000
+
+class miServidor(SimpleHTTPRequestHandler):
+    def do_GET (self):
+        if self.path =="/":
+            self.path= "/index.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
+       
+print("Servidor ejecutandose en el puerto 3000", port)
+server= HTTPServer(("localhost",port), miServidor)
+server.serve_forever()
+
