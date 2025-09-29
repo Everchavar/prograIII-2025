7b107d8a45c4b0ca0041e60c7025d1c070be4490
Everchavar <everchavarria07@gmail.com>
2025-09-29 17:17:45 -0600

    crud_modificado_terminado

---
+++ modulos/docentes.js
@@ -0,0 +1,98 @@
+var accion = "nuevo",
+    idDocente = 0;
+
+document.addEventListener("DOMContentLoaded", event=>{ 
+    frmDocentes.addEventListener("submit", e=>{
+        e.preventDefault();
+        guardarDocentes();
+    });
+    obtenerDocentes();
+});
+
+async function guardarDocentes(){
+    let datos = {
+        accion,
+        idDocente,
+        codigo: txtCodigoDocente.value,
+        nombre: txtNombreDocente.value,
+        direccion: txtDireccionDocente.value,
+        telefono: txtTelefonoDocente.value,
+        email: txtEmailDocente.value,
+        dui: txtDuiDocente.value,
+        escalafon: txtEscalafonDocente.value
+    };
+    let response = await fetch("/docentes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(datos),
+    }), 
+        respuesta = await response.json();
+
+    if(respuesta.msg!="ok"){
+        alertify.error(`Error al procesar docente: ${respuesta}`);
+        return;
+    }
+    limpiarFormulario();
+    obtenerDocentes();
+}
+
+function limpiarFormulario(){
+    accion = "nuevo";
+    idDocente = 0;
+    txtCodigoDocente.value = "";
+    txtNombreDocente.value = "";
+    txtDireccionDocente.value = "";
+    txtTelefonoDocente.value = "";
+    txtEmailDocente.value = "";
+    txtDuiDocente.value = "";
+    txtEscalafonDocente.value = "";
+}
+
+async function obtenerDocentes(){
+    let response = await fetch("/docentes"), 
+        respuesta = await response.json();
+    mostrarDatosDocentes(respuesta);
+}
+
+function mostrarDatosDocentes(docentes){
+    let filas = "";
+    docentes.forEach(docente=>{
+        filas += `
+            <tr onClick='mostrarDocente(${ JSON.stringify(docente) })'>
+                <td>${docente.codigo}</td>
+                <td>${docente.nombre}</td>
+                <td>${docente.direccion}</td>
+                <td>${docente.telefono}</td>
+                <td>${docente.email}</td>
+                <td>${docente.dui}</td>
+                <td>${docente.escalafon}</td>
+                <td>
+                    <button onClick='eliminarDocente(${ JSON.stringify(docente) }, event)' 
+                            class="btn btn-danger btn-sm">ELIMINAR</button>
+                </td>
+            </tr>
+        `;
+    });
+    tblDocentes.innerHTML = filas;
+}
+
+function mostrarDocente(docente){
+    accion = "modificar";
+    idDocente = docente.idDocente;
+    txtCodigoDocente.value = docente.codigo;
+    txtNombreDocente.value = docente.nombre;
+    txtDireccionDocente.value = docente.direccion;
+    txtTelefonoDocente.value = docente.telefono;
+    txtEmailDocente.value = docente.email;
+    txtDuiDocente.value = docente.dui;
+    txtEscalafonDocente.value = docente.escalafon;
+}
+
+function eliminarDocente(docente, event){
+    event.preventDefault();
+    if(confirm(`¿Está seguro de eliminar a ${docente.nombre}?`)){
+        idDocente = docente.idDocente;
+        accion = "eliminar";
+        guardarDocentes();
+    }
+}
